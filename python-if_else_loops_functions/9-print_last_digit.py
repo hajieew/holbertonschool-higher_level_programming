a7e96d37c109b0a43e64ce121d8f5ac4b2b61897
hajieew <hajieew@gmail.com>
2026-03-16 16:17:41 +0400

    Salim

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+def print_last_digit(number):
+    if number > 0:
+        last_digit = number % 10
+    else:
+        if number % 10 == 0:
+            last_digit = 0
+        else:
+            last_digit = -10 + number % 10
+    return last_digit
