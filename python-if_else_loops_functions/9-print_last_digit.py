14368009aaceb94883d104cc5eb9b7ce668bdfb4
hajieew <hajieew@gmail.com>
2026-03-19 00:41:33 +0400

    Salim

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,5 @@
 def print_last_digit(number):
  #   if number > 0:
   ########nt_last_digit(98))
-    return str(number)[-1]
-print(print_last_digit(98))
+    return int(str(number)[-1])
+print(print_last_digit(-98))
