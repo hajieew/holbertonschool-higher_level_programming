d22e8afa60e7813b89ad0872f997206f08b0e25a
hajieew <hajieew@gmail.com>
2026-03-19 17:17:55 +0400

    Salim

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if i >= 'A' and i <= 'Z':
+            print(i, end='')
+        else:
+            i = chr(ord(i) - 32)
+            print(i, end='')
+    print()
+
+str = 'HellO'
+uppercase(str)
