746ea0aee8e42c291565f3bcd091cb15ddc311b3
hajieew <hajieew@gmail.com>
2026-03-19 17:02:54 +0400

    Salim

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if len(c) != 1:
-        raise ValueError("Input must be a single character")
-    if 'a' <= c <= 'z':
+    if c >= 'a' and c <= 'z':
         return True
-    elif 'A' <= c <= 'Z':
-        return None
     else:
-        return None
+        return False
--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return abs(number) % 10
+    if number > 0:
+        last_digit = number % 10
+    else:
+        if number % 10 == 0:
+            last_digit = 0
+        else:
+            last_digit = -10 + number % 10
+    return last_digit
