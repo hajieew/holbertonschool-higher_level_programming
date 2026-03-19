c272ad6df870a17c20e9e7aa11095f9e1ab79dfe
hajieew <hajieew@gmail.com>
2026-03-19 17:37:19 +0400

    Salim

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print(last_digit)
+    last_digit = abs(number) % 10   # son rəqəm
+    print(last_digit, end='')       # çap et
     return last_digit
+
+# Funksiyanı sadəcə çağır
+print_last_digit(98)
