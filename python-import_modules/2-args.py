f43e78e7230e7580259459078e9925ad6deaa255
hajieew <hajieew@gmail.com>
2026-03-19 20:11:01 +0400

    Salim

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-from args_2 import *
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]  # arqumentlər (argv[0] faylın adı)
+    count = len(args)
+
+    # Number of arguments
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+    # Print each argument
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))
