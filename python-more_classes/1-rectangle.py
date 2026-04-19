9715113687b04049f74394d479f1647b89325199
hajieew <hajieew@gmail.com>
2026-04-19 19:02:15 +0400

    Salim

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class Rectangle:
+    """Represent a rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """initalize the attributes"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """return the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the value"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """return the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the value"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
