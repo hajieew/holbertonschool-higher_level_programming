344a00d0d8acc4a43a020217b633b7b513f8186d
hajieew <hajieew@gmail.com>
2026-04-19 19:44:23 +0400

    Salim

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that contains function pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """Find Pascal triangle"""
+
+    if n <= 0:
+        return []
+    else:
+        triangle = []
+        for i in range(n):
+            row = [1]
+    return 
