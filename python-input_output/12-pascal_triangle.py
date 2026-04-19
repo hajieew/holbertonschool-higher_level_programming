3ad97462cc90302774334f39dc7f885b9a5a6ea5
hajieew <hajieew@gmail.com>
2026-04-19 19:49:55 +0400

    Salim

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Module that contains function pascal_triangle"""
+"""abcde"""
 
 
 def pascal_triangle(n):
-    """Find Pascal triangle"""
-
+    """edcba"""
     if n <= 0:
         return []
-    else:
-        triangle = []
-        for i in range(n):
-            row = [1]
-  
-    return 
+    triangle = [[1]]
+    for i in range(1, n):
+        pre_row = triangle[i - 1]
+        curr_row = [1]
+        for j in range(1, i):
+            indiki_row.append(pre_row[j - 1] + pre_row[j])
+        curr_row.append(1)
+        triangle.append(curr_row)
+
+    return triangle
